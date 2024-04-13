630b009d5ce4eddfdb3011c7d98e876f3875e71b
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-04-13 22:42:01 +0900

    Initial Commit

---
+++ FaInt06NetflixStyle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaInt06NetflixStyle
+//
+//  Created by joe on 4/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
